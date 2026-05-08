8dea8fce87a8020917546039f06dd8f18a829b3e
Anil Kambar <anilatsas01@gmail.com>
2026-05-08 14:07:43 +0530

    Explore the project in deep

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,12 @@
+import Chai from "./Chai.js";
+
 function App() {
-  return <h1>chAI and React</h1>;
+  return (
+    <>
+      <h1>chAI and React | Anil Kambar</h1>
+      <Chai />
+    </>
+  );
 }
 
 export default App;
