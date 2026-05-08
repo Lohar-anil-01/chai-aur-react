4aaf257f41dc7043911f4531162103a78dd73a47
Anil Kambar <anilatsas01@gmail.com>
2026-05-08 14:09:49 +0530

    Explore deeply Vite project and know also about component and name extension

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,13 @@
+import Chai from "./chai.jsx";
+
 function App() {
-  return <h1>chAI aur React with Vite | Anil Kambar</h1>;
+  return (
+    <>
+      <Chai />
+      <h1>Chai Aur React</h1>
+      <p>Test Para</p>
+    </>
+  );
 }
 
 export default App;
